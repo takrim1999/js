c54da271efd21cdf16a6dd3392cf839be2cc7885
takrim1999 <takrimulalom@gmail.com>
2022-07-18 05:00:16 +0600

    drew a broken sphere

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ var context = canvas.getContext('2d');
 // size 300,150
 // context.moveTo(150,75);
 // context.lineTo(300,150);
-context.rect(0,0,100,50);
-context.rect(100,0,100,50);
-context.rect(200,0,100,50);
-context.rect(0,50,100,50);
-context.rect(0,100,100,50);
-context.rect(100,50,100,50);
-context.rect(200,50,100,50);
-context.rect(200,100,100,50);
-context.rect(100,100,100,50);
+context.arc(150,75,50,0,Math.PI*1.5);
+context.arc(200,25,50,Math.PI*0.5,Math.PI*1);
 context.stroke();
