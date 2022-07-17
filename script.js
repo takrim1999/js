5cba101cfee96f2550fef3c3566c9da36b257c93
takrim1999 <takrimulalom@gmail.com>
2022-07-18 04:40:59 +0600

    tictactoe board by using first canvas

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-enemy = document.getElementsByTagName('h1')[0];
-health = document.getElementsByTagName('span')[0];
-hitpoint = 100;
-health.innerText = hitpoint;
-
-function hit(){
-if (hitpoint>=0){
-    hitpoint = hitpoint-10;
-    health.innerText = hitpoint;
-    }
-if (hitpoint<0){
-    health.innerText = hitpoint;
-    hitpoint = 100;
-    alert("Game Over starting again!");
-    health.innerText = hitpoint;
-    }
-}
-
-
-enemy.onclick = hit;
-// health.innerText = hitpoint;
-// else{
-
-// }
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext('2d');
+// size 300,150
+// context.moveTo(150,75);
+// context.lineTo(300,150);
+context.rect(0,0,100,50);
+context.rect(100,0,100,50);
+context.rect(200,0,100,50);
+context.rect(0,50,100,50);
+context.rect(0,100,100,50);
+context.rect(100,50,100,50);
+context.rect(200,50,100,50);
+context.rect(200,100,100,50);
+context.rect(100,100,100,50);
+context.stroke();
