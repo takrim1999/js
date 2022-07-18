20fda4a467cf99d1b4eefa3f25213205819b1a37
takrim1999 <takrimulalom@gmail.com>
2022-07-18 12:56:12 +0600

    Better Canvas with better broken and sad sphere

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
-// size 300,150
+// size 600,600
 // context.moveTo(150,75);
 // context.lineTo(300,150);
-context.arc(150,75,50,0,Math.PI*1.5);
-context.arc(200,25,50,Math.PI*0.5,Math.PI*1);
+context.arc(300,300,250,0,Math.PI*1.5);
+context.arc(550,50,250,Math.PI*0.5,Math.PI*1);
 context.stroke();
