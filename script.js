fe0c68dad74c4a05ed65c654a8da76968da237b9
takrim1999 <takrimulalom@gmail.com>
2022-07-18 15:33:21 +0600

    index.html renamed

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
-// size 600,600
-// context.moveTo(150,75);
-// context.lineTo(300,150);
-context.arc(300,300,250,0,Math.PI*1.5);
-context.arc(550,50,250,Math.PI*0.5,Math.PI*1);
-context.stroke();
+
+var x = 100;
+var pos = 1;
+
+function clear(){
+    context.clearRect(0,0,600,600);
+}
+
+function square()
+{
+    context.beginPath();
+    context.rect(0,120,600,340);
+    context.fillStyle = "#0a4218";
+    context.stroke()
+    context.fill();
+}
+
+function circle(){
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.arc(300,300,x,0,Math.PI*2);
+    context.stroke();
+    context.fillStyle = 'red';
+    context.fill();
+};
+
+function animate(){
+    if (x>150 || x < 50){
+        pos = pos * (-1);
+    }
+    clear();
+    square();
+    circle();
+    x = x + pos;
+    
+}
+
+// text
+// function text(){
+//     context.beginPath();
+//     context.font = '42px Arial';
+//     context.fillStyle = '#FFFF00'
+//     context.strokeStyle = 'blue';
+//     context.lineWidth = 2;
+//     context.strokeText('Flag',270,500);
+//     context.fill();
+// };
+// context.beginPath();
+// var img = new Image();
+// img.src = 'https://blob.sololearn.com/avatars/sololearn.jpg';
+// context.drawImage(img,250,0,50,50);
+
+// setInterval(square,5);
+// setInterval(circle,5);
+// setInterval(text,5);
+
+setInterval(animate,10);
